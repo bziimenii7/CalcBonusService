988a5725b613000ea5b712aa0c709bfa2ba94979
Stepan <st.kulagin7@gmail.com>
2023-03-14 22:02:06 +0300

    initial commit

---
+++ src/test/java/ru/netology/javaqa/javaqamvn/services/BonusServiceTest.java
@@ -0,0 +1,18 @@
+package ru.netology.javaqa.javaqamvn.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+public class BonusServiceTest {
+
+    @ParameterizedTest
+    @CsvFileSource(files="src/test/resource/bonus.csv")
+    public void testRegisteredUnderLimit(int expected, int amount, boolean isRegistered) {
+        BonusService service = new BonusService();
+
+        int actual = service.calcBonus(amount, isRegistered);
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
